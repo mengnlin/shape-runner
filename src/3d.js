30690de424f8b8b2ac72788e1d3dc5d61e15f78a
mengna <linmengna9392@gmail.com>
2019-05-18 09:22:29 -0700

    add multi render and game over

--- src/3d.js
+++ src/3d.js
@@ -1,8 +1,10 @@
 import * as THREE from "three";
 const scene = new THREE.Scene();
+const sceneWidth = window.innerWidth / 2;
+const sceneHeight = window.innerHeight;
 const camera = new THREE.PerspectiveCamera(
   35,
-  window.innerWidth / window.innerHeight,
+  sceneWidth / sceneHeight,
   0.1,
   3000
 );
@@ -13,104 +15,139 @@ var light1 = new THREE.PointLight(0xffffff, 0.5);
 scene.add(light1);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(sceneWidth, sceneHeight);
 renderer.setClearColor(0xf8f3e9);
 document.body.appendChild(renderer.domElement);
 
-function createCube(x, y, z) {
-  const geometry = new THREE.BoxGeometry(50, 50, 50);
+const obstacleY = 600;
+const obstacleZ = -1000;
+
+function createCube(x, y = obstacleY) {
+  const geometry = new THREE.BoxGeometry(25, 25, 25);
   const material = new THREE.MeshLambertMaterial({ color: 0xef5462 });
   const cube = new THREE.Mesh(geometry, material);
   cube.position.x = x;
   cube.position.y = y;
-  cube.position.z = z;
+  cube.position.z = obstacleZ;
   return cube;
 }
-// create CUBE
-const obstacleCube1 = createCube(0, 600, -2200);
-scene.add(obstacleCube1);
 
-// create Sphere
-
-function createSphere(x, y, z) {
-  const geometry2 = new THREE.SphereGeometry(25);
+function createSphere(x, y = obstacleY) {
+  const geometry2 = new THREE.SphereGeometry(15);
   const material2 = new THREE.MeshLambertMaterial({ color: 0xef5462 });
   const sphere = new THREE.Mesh(geometry2, material2);
   sphere.position.x = x;
   sphere.position.y = y;
-  sphere.position.z = z;
+  sphere.position.z = obstacleZ;
   return sphere;
 }
-const obstacleSphere1 = createSphere(100, 600, -2200);
-scene.add(obstacleSphere1);
 
-// create Triangle
-function createTriangle(x, y, z, rx, rz) {
-  const geometry3 = new THREE.TetrahedronGeometry(45);
+function createTetra(x, y = obstacleY) {
+  const geometry3 = new THREE.TetrahedronGeometry(20);
   const material3 = new THREE.MeshLambertMaterial({ color: 0xef5462 });
-  const triangle = new THREE.Mesh(geometry3, material3);
-  triangle.rotation.x = 20;
-  triangle.rotation.z = -10;
-  triangle.position.x = x;
-  triangle.position.y = y;
-  triangle.position.z = z;
-  return triangle;
+  const Tetra = new THREE.Mesh(geometry3, material3);
+  Tetra.rotation.x = 20;
+  Tetra.rotation.z = -10;
+  Tetra.position.x = x;
+  Tetra.position.y = y;
+  Tetra.position.z = obstacleZ;
+  return Tetra;
 }
-const obstacleTriangle1 = createTriangle(-100, 600, -2200, 20, -10);
-scene.add(obstacleTriangle1);
 
-const obstacleShapes = [obstacleCube1, obstacleSphere1, obstacleTriangle1];
-// create User Object
-const UserCube = createCube(0, 0, -2200);
-const UserSphere = createSphere(0, 0, -2200);
-const UserTriangle = createTriangle(0, 0, -2200, 0, 0);
-const userShape = [UserCube, UserSphere, UserTriangle];
+let obstacleShapes = [];
+const UserCube = createCube(0, 0, -1000);
+const UserSphere = createSphere(0, 0, -1000);
+const UserTetra = createTetra(0, 0, -1000, 0, 0);
+const userShape = [
+  { type: "cube", object: UserCube },
+  { type: "sphere", object: UserSphere },
+  { type: "tetra", object: UserTetra }
+];
 let index = Math.floor(Math.random() * Math.floor(3));
 const UserRandomShape = userShape[index];
-scene.add(UserRandomShape);
+scene.add(UserRandomShape.object);
 
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
-  // console.log("keydown", event.which);
   var keyCode = event.which;
   if (keyCode == 37) {
-    UserRandomShape.position.x -= 100;
-    if (UserRandomShape.position.x <= -100) {
-      UserRandomShape.position.x = -100;
+    UserRandomShape.object.position.x -= 100;
+    if (UserRandomShape.object.position.x <= -100) {
+      UserRandomShape.object.position.x = -100;
     }
   } else if (keyCode == 39) {
-    UserRandomShape.position.x += 100;
-    if (UserRandomShape.position.x >= 100) {
-      UserRandomShape.position.x = 100;
+    UserRandomShape.object.position.x += 100;
+    if (UserRandomShape.object.position.x >= 100) {
+      UserRandomShape.object.position.x = 100;
     }
   }
 }
 function animate() {
   requestAnimationFrame(animate);
-  obstacleSphere1.position.y -= 1;
-  obstacleTriangle1.position.y -= 1;
-  obstacleCube1.position.y -= 1;
+  obstacleShapes.forEach(obstacleObject => {
+    obstacleObject.object.position.y -= 1;
+  });
 
   renderer.render(scene, camera);
-  gameOver(
-    UserRandomShape.position.x,
-    UserRandomShape.position.y,
-    obstacleShapes[index].position.x,
-    obstacleShapes[index].position.y
-  );
+  gameOver(UserRandomShape, obstacleShapes);
+  obstacleShapes = clearPassedShapes(obstacleShapes);
 }
+
+delay();
 animate();
 
-function gameOver(
-  UserRandomShapePositionX,
-  UserRandomShapePositionY,
-  obstacleShapesX,
-  obstacleShapesY
-) {
-  if (
-    UserRandomShapePositionX !== obstacleShapesX &&
-    obstacleShapesY === UserRandomShapePositionY
-  ) {
-    document.querySelector("div").classList.remove("hidden");
+function gameOver(UserRandomShape, obstacleShapes) {
+  obstacleShapes.forEach(obstacleObject => {
+    if (
+      UserRandomShape.object.position.x !== obstacleObject.object.position.x &&
+      obstacleObject.object.position.y === UserRandomShape.object.position.y &&
+      obstacleObject.type === UserRandomShape.type
+    ) {
+      document.querySelector("div").classList.remove("hidden");
+    }
+  });
+}
+
+function clearPassedShapes(obstacleShapes) {
+  return obstacleShapes
+    .map(obstacleObject => {
+      if (obstacleObject.object.position.y <= -200) {
+        scene.remove(obstacleObject.object);
+        return undefined;
+      } else {
+        return obstacleObject;
+      }
+    })
+    .filter(obstacleObject => obstacleObject !== undefined);
+}
+
+var shuffle = function(array) {
+  var currentIndex = array.length;
+  var temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
+  return array;
+};
+function delay() {
+  setTimeout(() => {
+    const xCoordinate = [0, 100, -100];
+    shuffle(xCoordinate);
+    const cube = createCube(xCoordinate[0]);
+    const sphere = createSphere(xCoordinate[1]);
+    const tetra = createTetra(xCoordinate[2]);
+    obstacleShapes.push(
+      { type: "cube", object: cube },
+      { type: "sphere", object: sphere },
+      { type: "tetra", object: tetra }
+    );
+    scene.add(cube, sphere, tetra);
+    delay();
+  }, 3000);
 }
